b5e55a96e288874bf47f2267ccbc0fc27d6ebaa4
ikmal <ikmalazman65@gmail.com>
2021-10-13 13:56:51 +0000

    New Line

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(void) {
 /* This is my first program in C */
-printf("Hello World!/n");
-printf("I Love C/n");
-printf("I love coding/n");
+printf("Hello World!\n");
+printf("I love coding\n");
 return (0);
 }
